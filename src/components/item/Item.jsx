2e5785363d041685fcfe4d03a6f43f2a3ced6fab
mustafa <yafess229@gmail.com>
2022-07-03 22:32:06 +0300

    update

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
 import "./Item.css";
 
-const Item = ({card}) => {
-    const {name, img, options} = card;
-  return (
-    <div className='item-container'>
-        <img className='item-img' src={img} alt="" />
-        <h3 className='item-title'>{name}</h3>
-    </div>
-  )
+const Item = ({ card }) => {
+    const [show, setShow] = useState(true);
+    const handleClick = ()=>{
+        setShow(!show)
+    }
+    const { name, img, options } = card;
+    return (
+        <div className='item-container' onClick={handleClick}>
+          
+         {show ? 
+         
+         <div>
+            <img className='item-img' src={img} alt="" />
+            <h3 className='item-title'>{name}</h3>
+            </div> 
+            : 
+            
+            <ul>
+            {options.map((element,index)=>{
+                return(
+                    <li key={index}>  {element}   </li >
+                )
+            })}
+            </ul>  }
+          
+
+
+            
+
+
+         
+            
+        </div>
+    )
 }
 
 export default Item
